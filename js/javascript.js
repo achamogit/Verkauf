d9b9bb53ef74fa3de9741d9a23a07f3057c34a61
achamogit <iachamo@gmail..com>
2016-01-19 18:44:24 -0500

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+
+
+	function pasuser(form) {
+		if (form.id.value=="usuario") { 
+		if (form.pass.value=="lima32") {              
+		location="indicium.html" 
+		} else {
+		alert("Password Incorrecto")
+		}
+		} else {  alert("Usuario Invalido")
+		}
+	}
+
+
